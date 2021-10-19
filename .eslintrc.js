d94be0f2be5de9b2ddb38ab00e933e4b6b992730
Scott Tran <scotttran506@yahoo.com>
2021-10-19 00:51:53 -0500

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,5 @@ module.exports = {
     'airbnb/hooks',
   ],
   rules: {
-    //   'no-console': 'error',
-    //   'react/react-in-jsx-scope': 'off',
-    //   'react/prop-types': 'off',
-    //   'simple-import-sort/imports': 'error',
-    //   'simple-import-sort/exports': 'error',
-    //   'unicorn/filename-case': 'off',
-    //   'jsx-a11y/anchor-is-valid': 'off',
-    //   'unicorn/prevent-abbreviations': 'off',
-    //   'react/no-unescaped-entities': 'off'
   },
 };
